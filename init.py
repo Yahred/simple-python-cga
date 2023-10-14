ac2dacb261cb9f298bf6c1c313e5bf9e7575d397
Yahred <dr.yahred@outlook.es>
2023-10-14 03:53:20 -0700

    correccio

--- init.py
+++ init.py
@@ -1,10 +1,1 @@
 from cga import evolve
-
-def fitness(bin: str):
-    ind = int(bin, 2)
-    return ind
-
-
-best = evolve(fitness, num_genes=10, generations=10000, fitness_min=True)
-
-print(best)
